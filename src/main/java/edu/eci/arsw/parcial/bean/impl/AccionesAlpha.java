d23bc806e94a5fb9f302a2f41719a7ea37115cd3
Jonathan Prieto <jonnhi.prietou@gmail.com>
2018-10-25 12:31:41 -0500

    Cache implementado

--- src/main/java/edu/eci/arsw/parcial/bean/impl/AccionesAlpha.java
+++ src/main/java/edu/eci/arsw/parcial/bean/impl/AccionesAlpha.java
@@ -8,6 +8,7 @@ package edu.eci.arsw.parcial.bean.impl;
 import edu.eci.arsw.parcial.Model.HttpConnection;
 import edu.eci.arsw.parcial.bean.Acciones;
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,9 +18,22 @@ import org.springframework.stereotype.Service;
 @Service("Alpha")
 public class AccionesAlpha implements Acciones {
 
+    private ConcurrentHashMap<String, String> cache = new ConcurrentHashMap<String, String>();
+    private ConcurrentHashMap<String, String> contenido = new ConcurrentHashMap<String, String>();
+
     @Override
     public String obtenerAcciones(String rango, String nameAccion) throws IOException {
-        return HttpConnection.getUrlData("https://www.alphavantage.co/query?function=" + rango + "&symbol=" + nameAccion + "&interval=5min&apikey=Q1QZFVJQ21K7C6XM");
+        String urlData;
+        if (cache.containsKey(rango) && cache.containsValue(nameAccion)) {
+            System.out.println("Lo tiene");
+            urlData = contenido.get(rango + nameAccion);
+        } else {
+            System.out.println("No lo tiene");
+            urlData = HttpConnection.getUrlData("https://www.alphavantage.co/query?function=" + rango + "&symbol=" + nameAccion + "&interval=5min&apikey=Q1QZFVJQ21K7C6XM");
+            cache.put(rango, nameAccion);
+            contenido.put(rango + nameAccion, urlData);
+        }
+        return urlData;
     }
 
 }
